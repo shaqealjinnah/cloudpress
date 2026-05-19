1b840ab683aeb9fd1b0f1aebcf4d1684a4e54165
shaqealjinnah <j.shaqeal@gmail.com>
2026-05-19 17:51:40 +1000

    feat: update user-data script to include health endpoint

--- scripts/install_wordpress.sh
+++ scripts/install_wordpress.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
-# Update packages
-apt-get update -y
-apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get install -y apache2 php php-mysql mysql-client
 
-# Install Apache, MySQL, PHP
-apt-get install -y apache2 php php-mysql libapache2-mod-php mysql-server wget unzip
+# Start and enable Apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
-# Start services
-systemctl start apache2
-systemctl enable apache2
-systemctl start mysql
-systemctl enable mysql
+# Download and configure WordPress
+cd /tmp
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html/
+sudo rm -rf wordpress latest.tar.gz
 
-# MySQL Setup
-mysql -e "CREATE DATABASE wordpress;"
-mysql -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'StrongPassword123';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+# Set correct permissions
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
 
-# Install WordPress
-cd /var/www/html
-rm -f index.html
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-cp -r wordpress/* .
-rm -rf wordpress latest.tar.gz
+# Configure wp-config.php
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-# Configure WordPress
-cp wp-config-sample.php wp-config.php
-sed -i "s/database_name_here/wordpress/" wp-config.php
-sed -i "s/username_here/wpuser/" wp-config.php
-sed -i "s/password_here/StrongPassword123/" wp-config.php
+# Update WordPress configuration
+sudo sed -i "s/database_name_here/${db_name}/" /var/www/html/wp-config.php
+sudo sed -i "s/username_here/${db_username}/" /var/www/html/wp-config.php
+sudo sed -i "s/password_here/${db_password}/" /var/www/html/wp-config.php
+sudo sed -i "s/localhost/${db_endpoint}/" /var/www/html/wp-config.php
 
-# Set permissions
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+# Remove default Apache index.html
+sudo rm -f /var/www/html/index.html
 
-# Enable Apache rewrite
-a2enmod rewrite
-systemctl restart apache2
+# Add heath endpoint for alb
+echo "ok" | sudo tee /var/www/html/health.html
+
+# Restart Apache2
+sudo systemctl restart apache2
